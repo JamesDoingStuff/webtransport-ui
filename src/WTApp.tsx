69e9948acda88b5409856aabb38674931790ab43
James Gilbert <james.gilbert@diamond.ac.uk>
2025-08-12 10:27:49 +0000

    Changed vars to useRef

--- src/WTApp.tsx
+++ src/WTApp.tsx
@@ -1,25 +1,21 @@
 // import { useEffect, useState } from 'react'
 import { Button, Stack, TextField } from '@mui/material';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 
 
 function App() { 
   const [address, setAddress] = useState("https://localhost:4433/tempcontroller");
   const [data, setData] = useState("Test data")
-  // const [wt, setWt] = useState<WebTransport | null>(null);
-  // const [isConnected, setIsConnected] = useState(false);
-  // const [writer, setWriter] = useState<WritableStreamDefaultWriter | null>(null)
-  let wt: WebTransport | null = null;
-  let writer: WritableStreamDefaultWriter | null = null;
+  const wt = useRef<WebTransport|null>(null);
+  const writer = useRef<WritableStreamDefaultWriter|null>(null);
 
   async function initTransport(url: string) {
-    wt = new WebTransport(url);
+    wt.current = new WebTransport(url);
 
-    if (wt) {await wt.ready}
+    if (wt.current) {await wt.current.ready}
     else console.log("Wt not set")
-    
-    // setIsConnected(true)
+
     console.log("Transport created")
     return;
   }
@@ -35,23 +31,23 @@ function App() {
 
   async function closeWriter(writer: WritableStreamDefaultWriter | null) {
     if (writer) await writer.close()
-    if (wt)  {
-      wt.close();
-      await wt.closed
+    if (wt.current)  {
+      wt.current.close();
+      await wt.current.closed
     }
   }
 
   async function connect(url: string) {
     await initTransport(url);
     // setWt(transport)
-    if (!wt) {
+    if (!wt.current) {
       console.log("Null transport");
       return;
     }
     
-    const stream = await wt.createBidirectionalStream();
+    const stream = await wt.current.createBidirectionalStream();
 
-    writer = stream.writable.getWriter();
+    writer.current = stream.writable.getWriter();
     const reader = stream.readable.getReader();
 
     async function read() {
@@ -90,12 +86,12 @@ function App() {
           onChange={(e) => setData(e.target.value)}
         />
         <Button
-          onClick={() => sendWriterData(writer)}
+          onClick={() => sendWriterData(writer.current)}
         >
           Send test stream data
         </Button>
         <Button
-          onClick={() => closeWriter(writer)}
+          onClick={() => closeWriter(writer.current)}
         >
           Close connection
         </Button>
