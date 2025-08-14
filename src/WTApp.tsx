03e0938a8c089847d6b15e7e969b56b0dea9061c
James Gilbert <james.gilbert@diamond.ac.uk>
2025-08-14 12:24:22 +0000

    Use sci-react-ui and improve shutdown behaviour

--- src/WTApp.tsx
+++ src/WTApp.tsx
@@ -1,12 +1,18 @@
 // import { useEffect, useState } from 'react'
 import { Button, Stack, TextField } from '@mui/material';
 import { useRef, useState } from 'react';
+import { AppBar, Toolbar } from '@mui/material';
+import { Typography } from '@mui/material';
+import { Footer } from '@diamondlightsource/sci-react-ui';
+import { styled } from '@mui/material/styles'
 
+const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
 function App() { 
   const [address, setAddress] = useState("https://localhost:4433/tempcontroller");
-  const [data, setData] = useState("12")
+  const [data, setData] = useState("0")
   const [currentValue, setCurrentValue] = useState("0")
+  const [isConnected, setIsConnected] = useState(false)
   const wt = useRef<WebTransport|null>(null);
   const writer = useRef<WritableStreamDefaultWriter|null>(null);
 
@@ -14,7 +20,7 @@ function App() {
     wt.current = new WebTransport(url);
 
     if (wt.current) {await wt.current.ready}
-    else console.log("Wt not set")
+    else console.log("Transport does not exist")
 
     console.log("Transport created")
     return;
@@ -29,18 +35,37 @@ function App() {
     }
   }
 
-  async function closeWriter(writer: WritableStreamDefaultWriter | null) {
-    if (writer) await writer.close()
-    if (wt.current)  {
-      wt.current.close();
-      await wt.current.closed
+  async function closeWriter() {
+
+    if (writer.current) {
+      await writer.current.close().then(() => console.log("Closed writer.")
+      ).catch(() => console.log("Writer went badly."))
+      
+      writer.current = null
     }
+      
+    if (wt.current)  {
+      wt.current.close({
+        closeCode: 17,
+        reason: "CloseButtonPressed",
+      });
+      await wt.current.closed.then(() => console.log("Closed transport gracefully.")
+      ).catch((err) => console.log(`Transport died kicking and screaming: ${err}`))
+      wt.current = null
+    }
+
+    setIsConnected(false);
   }
 
   async function connect(url: string) {
-    await initTransport(url);
+    try {await initTransport(url)}
+    catch {
+      console.log("Failed to create transport");
+      return;
+    }
+  
     if (!wt.current) {
-      console.log("Null transport");
+      console.log("No transport");
       return;
     }
     
@@ -49,10 +74,10 @@ function App() {
     writer.current = stream.writable.getWriter();
     const reader = stream.readable.getReader();
 
+    setIsConnected(true);
     async function read() {
       const decoder = new TextDecoder();
       while (true) {
-        console.log("Loop executed")
         const { value, done } = await reader.read();
         if (done) {
           break
@@ -69,41 +94,60 @@ function App() {
 
 
   return (
-        <Stack>
+    <>
+      <AppBar color='primary' position='static'>
+        <Toolbar>
+          <Typography>
+            WebTransport Demo
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Offset/>
+      <Stack spacing={2}>
         <TextField 
           label="Device path"
           defaultValue={address}
           onChange={(e) => setAddress(e.target.value)}
         />
-        <Button
-          onClick={() => connect(address)}
-        >
-          Connect
-        </Button>
-        <Stack direction='row'>
-          <TextField
-            value={currentValue}
-            label="Temperature"
-            fullWidth
-          />
-          <TextField
-            label="Set Temperature"
-            defaultValue="293"
-            onChange={(e) => setData(e.target.value)}
-            fullWidth
-          />
-        </Stack>
-        <Button
+        <Typography align='center'>
+          <Button
+            onClick={() => connect(address)}
+            variant='contained'
+            style={{ 
+              maxWidth: "100px",
+            }}
+            disabled={isConnected}
+          >
+            Connect
+          </Button>
+          <Button 
+            onClick={() => closeWriter()}
+            disabled={!isConnected}
+          >
+            Close connection
+          </Button>
+        </Typography>
+      <Stack direction='row'>
+        <TextField
+          value={currentValue}
+          label="Temperature"
+          fullWidth
+        />
+        <TextField
+          label="Set Temperature"
+          defaultValue="0"
+          onChange={(e) => setData(e.target.value)}
+          fullWidth
+        />
+      </Stack>
+      <Button
           onClick={() => sendWriterData(writer.current)}
         >
           Send test stream data
-        </Button>
-        <Button
-          onClick={() => closeWriter(writer.current)}
-        >
-          Close connection
-        </Button>
-        </Stack>
+      </Button>
+      </Stack>
+      <Footer logo="theme" color="primary" position='fixed' width="100%"/>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider, DiamondTheme } from '@diamondlightsource/sci-react-ui'
 import App from './WTApp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={DiamondTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
