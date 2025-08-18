148df7cb37b0b786828fc0f18ea318337dbbccfe
James Gilbert <james.gilbert@diamond.ac.uk>
2025-08-18 10:50:54 +0100

    Start separating components

--- src/WTApp.tsx
+++ src/WTApp.tsx
@@ -7,21 +7,115 @@ import { styled } from '@mui/material/styles'
 
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
-function App() { 
+function ConnectionField(setReader, setWriter) {
   const [address, setAddress] = useState("https://localhost:4433/tempcontroller");
-  const [data, setData] = useState("0")
-  const [currentValue, setCurrentValue] = useState("0")
   const [isConnected, setIsConnected] = useState(false)
-  const wt = useRef<WebTransport|null>(null);
-  const writer = useRef<WritableStreamDefaultWriter|null>(null);
 
-  async function initTransport(url: string) {
-    wt.current = new WebTransport(url);
-    await wt.current.ready
+  async function initTransport() {
+    const wt = new WebTransport(address);
+    await wt.ready
 
     console.log("Transport created")
-    return;
+    return wt;
   }
+
+  async function connect() {
+    let wt = null
+    try {wt = await initTransport()}
+    catch {
+      console.log("Failed to create transport");
+      return;
+    }
+  
+    if (!wt) {
+      console.log("No transport");
+      return;
+    }
+    setIsConnected(true);
+    const stream = await wt.createBidirectionalStream();
+
+    setWriter(stream.writable.getWriter())
+    setReader(stream.readable.getReader())
+  }
+
+
+  async function closeConnection(writer) {
+
+    if (writer) {
+      await writer.close().then(() => console.log("Closed writer.")
+      ).catch(() => console.log("Writer went badly."))
+      
+      setWriter(null)
+    }
+      
+    // if (wt)  {
+    //   wt.close({
+    //     closeCode: 17,
+    //     reason: "CloseButtonPressed",
+    //   });
+    //   await wt.closed.then(() => console.log("Closed transport gracefully.")
+    //   ).catch((err) => console.log(`Transport died kicking and screaming: ${err}`))
+    //   wt = null
+    }
+
+    setIsConnected(false);
+  
+    
+  //   async function read() {
+  //     const decoder = new TextDecoder();
+  //     while (true) {
+  //       const { value, done } = await reader.read();
+  //       if (done) {
+  //         break
+  //       }
+  //       const received_data = decoder.decode(value, {stream: true});
+  //       console.log(`Value received: ${received_data}\n`)
+  //       setCurrentValue(received_data);
+  //     }
+  //   }
+
+  //   read();
+  //   return;
+  // }
+
+  return(
+    <>
+    <TextField 
+        label="Device path"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+      />
+      <Typography align='center'>
+        <Button
+          onClick={() => connect()}
+          variant='contained'
+          style={{ 
+            maxWidth: "100px",
+          }}
+          disabled={isConnected}
+        >
+          Connect
+        </Button>
+        <Button 
+          onClick={() => closeConnection()}
+          disabled={!isConnected}
+        >
+          Close connection
+        </Button>
+      </Typography>
+    </>
+  )
+}
+
+function App() { 
+  const [data, setData] = useState("0")
+  const [currentValue, setCurrentValue] = useState("0")
+  
+  const [wt, setWt] = useState<WebTransport|null>(null);
+  const [writer, setWriter] = useState<WritableStreamDefaultWriter|null>(null);
+  const [reader, setReader] = useState<ReadableStreamDefaultReader|null>(null);
+
+  
 
   function sendWriterData(writer: WritableStreamDefaultWriter | null) {
     const encoder = new TextEncoder()
@@ -32,62 +126,7 @@ function App() {
     }
   }
 
-  async function closeConnection() {
-
-    if (writer.current) {
-      await writer.current.close().then(() => console.log("Closed writer.")
-      ).catch(() => console.log("Writer went badly."))
-      
-      writer.current = null
-    }
-      
-    if (wt.current)  {
-      wt.current.close({
-        closeCode: 17,
-        reason: "CloseButtonPressed",
-      });
-      await wt.current.closed.then(() => console.log("Closed transport gracefully.")
-      ).catch((err) => console.log(`Transport died kicking and screaming: ${err}`))
-      wt.current = null
-    }
-
-    setIsConnected(false);
-  }
-
-  async function connect(url: string) {
-    try {await initTransport(url)}
-    catch {
-      console.log("Failed to create transport");
-      return;
-    }
   
-    if (!wt.current) {
-      console.log("No transport");
-      return;
-    }
-    
-    const stream = await wt.current.createBidirectionalStream();
-
-    writer.current = stream.writable.getWriter();
-    const reader = stream.readable.getReader();
-
-    setIsConnected(true);
-    async function read() {
-      const decoder = new TextDecoder();
-      while (true) {
-        const { value, done } = await reader.read();
-        if (done) {
-          break
-        }
-        const received_data = decoder.decode(value, {stream: true});
-        console.log(`Value received: ${received_data}\n`)
-        setCurrentValue(received_data);
-      }
-    }
-
-    read();
-    return;
-  }
 
 
   return (
@@ -101,29 +140,7 @@ function App() {
       </AppBar>
       <Offset/>
       <Stack spacing={2}>
-        <TextField 
-          label="Device path"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-        <Typography align='center'>
-          <Button
-            onClick={() => connect(address)}
-            variant='contained'
-            style={{ 
-              maxWidth: "100px",
-            }}
-            disabled={isConnected}
-          >
-            Connect
-          </Button>
-          <Button 
-            onClick={() => closeConnection()}
-            disabled={!isConnected}
-          >
-            Close connection
-          </Button>
-        </Typography>
+      <ConnectionField setReader={setReader} setWriter={setWriter}/>
       <Stack direction='row'>
         <TextField
           value={currentValue}
@@ -138,7 +155,7 @@ function App() {
         />
       </Stack>
       <Button
-          onClick={() => sendWriterData(writer.current)}
+          onClick={() => sendWriterData(writer)}
         >
           Send test stream data
       </Button>
